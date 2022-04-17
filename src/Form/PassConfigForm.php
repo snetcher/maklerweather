5e41398b71a87a6267c164e0323abc94b5810e89
Constantine <snetcher@gmail.com>
2022-04-17 15:02:16 +0300

    Fix: types of the functions return values

--- src/Form/PassConfigForm.php
+++ src/Form/PassConfigForm.php
@@ -13,21 +13,21 @@ class PassConfigForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public function getFormId(): string {
     return 'maklerweather_appid_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
+  protected function getEditableConfigNames(): array {
     return ['maklerweather.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state): array {
     $form = parent::buildForm($form, $form_state);
     $form['appid'] = [
       '#type' => 'textfield',
